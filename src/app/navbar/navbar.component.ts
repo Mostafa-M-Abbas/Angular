5a709653230f3d2861cbd40916c335f2a89d3bc3
Mostafa-M-Abbas <90660482+Mostafa-M-Abbas@users.noreply.github.com>
2024-07-30 01:20:48 +0300

    Update navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  imgUrl: string = '../assets/logo/logo.jpeg';
 }
